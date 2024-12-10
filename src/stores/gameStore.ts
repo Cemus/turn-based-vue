b77b1c3d4a430dcec0ca6cd66ed976faa83ba91f
Cemus <24947582+Cemus@users.noreply.github.com>
2024-12-10 17:01:12 +0100

    Fix component

--- src/stores/gameStore.ts
+++ src/stores/gameStore.ts
@@ -3,26 +3,47 @@ import { ref } from 'vue'
 import { type GameState } from '@/types'
 
 export const useGameStore = defineStore('gameStore', () => {
-  const socketUrl: string = 'http://localhost:3000/'
+  const socketUrl: string = 'ws://localhost:3000/'
   const socket = ref<WebSocket | null>(null)
   const gameState = ref<GameState | null>(null)
+  const isConnected = ref<boolean>(false)
+  const playerId = ref<string | null>(null)
 
   const connectWebSocket = () => {
+    if (socket.value) {
+      socket.value.close()
+    }
+
     socket.value = new WebSocket(socketUrl)
+
     socket.value.onopen = () => {
       console.log('WebSocket connecté')
+      isConnected.value = true
     }
+
     socket.value.onerror = (error) => {
       console.error('Erreur WebSocket :', error)
+      isConnected.value = false
     }
+
     socket.value.onclose = () => {
       console.log('Déconnecté du WebSocket')
+      isConnected.value = false
+    }
+
+    socket.value.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      if (data.type === 'connected') {
+        playerId.value = data.playerId
+      }
     }
   }
 
   return {
     socket,
-    connectWebSocket,
     gameState,
+    isConnected,
+    playerId,
+    connectWebSocket,
   }
 })
